78e13b3850a7a21bcce4b9f9255eadac3e92fb40
Abashin Aleksey <mmilasomm@gmail.com>
2022-10-25 20:45:54 +0300

    делалем дз

---
+++ dz/Program.cs
@@ -0,0 +1,23 @@
+﻿int size = 6;
+string[] array = new string[size];
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Введите любые набор символов :");
+for (int i = 0; i < size; i++)
+{
+    array[i] = Console.ReadLine();
+}
+for (int i = 0; i < size; i++)
+{
+
+    Console.Write(" " + array[i]);
+}
+Console.WriteLine();
+Console.WriteLine(" строковый массив меньше трёх");
+for (int i = 0; i < size; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
